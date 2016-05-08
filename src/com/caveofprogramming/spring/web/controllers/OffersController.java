19e588c35afb52b1a20114ff8c527936b9c4cc09
Rahul John Louis <rahul.louis@gmail.com>
2016-05-08 19:50:21 -0400

    Added TestFilter to intercept all requests

---
+++ src/com/caveofprogramming/spring/web/controllers/OffersController.java
@@ -0,0 +1,72 @@
+package com.caveofprogramming.spring.web.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.caveofprogramming.spring.web.dao.Offer;
+import com.caveofprogramming.spring.web.service.OffersService;
+
+@Controller
+public class OffersController {
+
+	private OffersService offerService;
+
+	@Autowired
+	public void setOfferService(OffersService offerService) {
+		this.offerService = offerService;
+	}
+
+	@RequestMapping("/offers")
+	public String showOffers(Model model) {
+		List<Offer> offers = offerService.getCurrent();
+		//offerService.throwTestException();
+		model.addAttribute("offers", offers);
+
+		return "offers";
+	}
+	
+	@RequestMapping("/createoffer")
+	public String createOffer(Model model) {
+		model.addAttribute("offer",new Offer());
+		return "createoffer";
+	}
+	
+	@RequestMapping(value="/docreate", method=RequestMethod.POST)
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+		
+		if(result.hasErrors())
+		{
+			System.out.println("Form does not validate");
+			List<ObjectError> errors = result.getAllErrors();
+			for(ObjectError error: errors)
+			{
+				System.out.println(error.getDefaultMessage());
+			}
+			return "createoffer";
+		}
+		
+		offerService.create(offer);
+		
+		return "offercreated";
+	}
+	
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String showTest(Model model,@RequestParam("id")String id) {
+		System.out.println(id);
+		
+		return "home";
+	}
+	
+}
